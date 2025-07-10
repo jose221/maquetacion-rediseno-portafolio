f5c0b35f8c94b5e4dfd22430433dbf422a2d0549
jose221 <ext.angel.alvarado@pricetravel.com>
2025-07-10 00:27:13 -0500

    Update static token in `TokenService` with new domain endpoint

--- src/services/TokenService.php
+++ src/services/TokenService.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../helpers/CurlHelper.php';
 class TokenService {
     public static function generateToken() {
         //$token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtb2RlIjoiIGRldmVsb3BtZW50IiwiZG9tYWluIjoiIGh0dHA6Ly9sb2NhbGhvc3Q6ODAwMCIsInVzZXJfaWQiOiIxIiwiaWF0IjoxNzUwNjQ5NDExfQ.T8MFbldCdISAfdB2HMNgfkZrXAdKhGQWE9avrH5udxA";
-        $token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtb2RlIjoiIHByb2R1Y3Rpb24iLCJkb21haW4iOiIgaHR0cHM6Ly93d3cuaGVyYW5kcm8ubGF0IiwidXNlcl9pZCI6IjEiLCJpYXQiOjE3NTA2NDk0MzF9.-VtKhQXgVXhzIT1WzKSKv8OaU3Zx_UQYQsABhhl0cxg";
+        $token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtb2RlIjoiIHByb2R1Y3Rpb24iLCJkb21haW4iOiIgaHR0cHM6Ly9qb3NlYWx2YXJhZG8uaGVyYW5kcm8ubGF0IiwidXNlcl9pZCI6IjEiLCJpYXQiOjE3NTIxMjUyMTN9.pODNKE9cHAKT1wfD7OjZ1A0Cg66H_vAxv-AOFoAurHg";
 
         return $token;
     }
