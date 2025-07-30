ee31f3b78769edd705e7824ce1c25ab297456ce6
jose221 <ext.angel.alvarado@pricetravel.com>
2025-07-29 22:49:02 -0500

    Update static token in `TokenService` with refreshed timestamp.

--- src/services/TokenService.php
+++ src/services/TokenService.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../helpers/CurlHelper.php';
 class TokenService {
     public static function generateToken() {
         //$token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtb2RlIjoiIGRldmVsb3BtZW50IiwiZG9tYWluIjoiIGh0dHA6Ly9sb2NhbGhvc3Q6ODAwMCIsInVzZXJfaWQiOiIxIiwiaWF0IjoxNzUwNjQ5NDExfQ.T8MFbldCdISAfdB2HMNgfkZrXAdKhGQWE9avrH5udxA";
-        $token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtb2RlIjoiIHByb2R1Y3Rpb24iLCJkb21haW4iOiIgaHR0cHM6Ly9qb3NlYWx2YXJhZG8uaGVyYW5kcm8ubGF0IiwidXNlcl9pZCI6IjEiLCJpYXQiOjE3NTM4MzAyNzJ9.kCcwdkiST7hObyacKWC2g1GJmlTh5YBJOYs26JSwTRk";
+        $token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtb2RlIjoicHJvZHVjdGlvbiIsImRvbWFpbiI6Imh0dHBzOi8vam9zZWFsdmFyYWRvLmhlcmFuZHJvLmxhdCIsInVzZXJfaWQiOiIxIiwiaWF0IjoxNzUzODQ3MzAyfQ.5LN-MT_x-FRPoQdmXuphIRhZa9gHbSc6ldGSkpEgv4I";
         return $token;
     }
 }
